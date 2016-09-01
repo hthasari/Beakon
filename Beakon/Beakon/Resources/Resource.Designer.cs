6c9048ae80d0b624d74addfb35f6d69fc55896b5
Ian <maskellx@gmail.com>
2016-09-01 22:22:45 +1000

    axml fixes

--- Beakon/Beakon/Resources/Resource.Designer.cs
+++ Beakon/Beakon/Resources/Resource.Designer.cs
@@ -78,47 +78,110 @@ namespace Beakon
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000d
-			public const int MyListView = 2131034125;
-			
-			// aapt resource value: 0x7f050001
-			public const int basicText = 2131034113;
-			
-			// aapt resource value: 0x7f050009
-			public const int imageButton1 = 2131034121;
-			
-			// aapt resource value: 0x7f05000b
-			public const int imageButton2 = 2131034123;
-			
-			// aapt resource value: 0x7f05000c
-			public const int imageButton3 = 2131034124;
-			
-			// aapt resource value: 0x7f050002
-			public const int imageView1 = 2131034114;
-			
-			// aapt resource value: 0x7f050007
-			public const int linearLayout1 = 2131034119;
-			
-			// aapt resource value: 0x7f050006
-			public const int listView = 2131034118;
-			
-			// aapt resource value: 0x7f050000
-			public const int messageHeader = 2131034112;
-			
-			// aapt resource value: 0x7f050008
-			public const int relativeLayout1 = 2131034120;
+			// aapt resource value: 0x7f050011
+			public const int MyListView = 2131034129;
 			
 			// aapt resource value: 0x7f050005
-			public const int searchView1 = 2131034117;
-			
-			// aapt resource value: 0x7f05000a
-			public const int toolbar1 = 2131034122;
+			public const int abtnNotification = 2131034117;
 			
 			// aapt resource value: 0x7f050004
-			public const int txtLastName = 2131034116;
+			public const int abtnRequest = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int abtnSettings = 2131034118;
+			
+			// aapt resource value: 0x7f050009
+			public const int btnNotification = 2131034121;
+			
+			// aapt resource value: 0x7f050008
+			public const int btnRequest = 2131034120;
+			
+			// aapt resource value: 0x7f05000a
+			public const int btnSettings = 2131034122;
+			
+			// aapt resource value: 0x7f05000d
+			public const int imageButton1 = 2131034125;
+			
+			// aapt resource value: 0x7f05000f
+			public const int imageButton2 = 2131034127;
+			
+			// aapt resource value: 0x7f050010
+			public const int imageButton3 = 2131034128;
+			
+			// aapt resource value: 0x7f050000
+			public const int imageView1 = 2131034112;
+			
+			// aapt resource value: 0x7f05000b
+			public const int linearLayout1 = 2131034123;
+			
+			// aapt resource value: 0x7f050016
+			public const int linearLayout2 = 2131034134;
+			
+			// aapt resource value: 0x7f05001a
+			public const int linearLayout3 = 2131034138;
+			
+			// aapt resource value: 0x7f050012
+			public const int linearLayout4 = 2131034130;
+			
+			// aapt resource value: 0x7f05001e
+			public const int linearLayout5 = 2131034142;
 			
 			// aapt resource value: 0x7f050003
-			public const int txtName = 2131034115;
+			public const int linearLayout6 = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int listView = 2131034119;
+			
+			// aapt resource value: 0x7f05000c
+			public const int relativeLayout1 = 2131034124;
+			
+			// aapt resource value: 0x7f050014
+			public const int space1 = 2131034132;
+			
+			// aapt resource value: 0x7f050018
+			public const int space2 = 2131034136;
+			
+			// aapt resource value: 0x7f05001c
+			public const int space3 = 2131034140;
+			
+			// aapt resource value: 0x7f050021
+			public const int switch1 = 2131034145;
+			
+			// aapt resource value: 0x7f050020
+			public const int textAutoCal = 2131034144;
+			
+			// aapt resource value: 0x7f050022
+			public const int textAutoLoc = 2131034146;
+			
+			// aapt resource value: 0x7f05001b
+			public const int textEmail = 2131034139;
+			
+			// aapt resource value: 0x7f05001d
+			public const int textEmailInput = 2131034141;
+			
+			// aapt resource value: 0x7f05001f
+			public const int textFavCont = 2131034143;
+			
+			// aapt resource value: 0x7f050013
+			public const int textFirstName = 2131034131;
+			
+			// aapt resource value: 0x7f050015
+			public const int textFirstNameInput = 2131034133;
+			
+			// aapt resource value: 0x7f050017
+			public const int textLastName = 2131034135;
+			
+			// aapt resource value: 0x7f050019
+			public const int textLastNameInput = 2131034137;
+			
+			// aapt resource value: 0x7f05000e
+			public const int toolbar1 = 2131034126;
+			
+			// aapt resource value: 0x7f050002
+			public const int txtLastName = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int txtName = 2131034113;
 			
 			static Id()
 			{
@@ -134,19 +197,19 @@ namespace Beakon
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int CustomListView = 2130903040;
+			public const int listView_row = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int listView_row = 2130903041;
+			public const int Main = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int Main = 2130903042;
+			public const int members = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int members = 2130903043;
+			public const int Notification = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int Notification = 2130903044;
+			public const int settings = 2130903044;
 			
 			// aapt resource value: 0x7f030005
 			public const int Template = 2130903045;
@@ -169,6 +232,24 @@ namespace Beakon
 			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040006
+			public const int SettingsCalendarAuto = 2130968582;
+			
+			// aapt resource value: 0x7f040004
+			public const int SettingsEmail = 2130968580;
+			
+			// aapt resource value: 0x7f040005
+			public const int SettingsFavCont = 2130968581;
+			
+			// aapt resource value: 0x7f040002
+			public const int SettingsFirstName = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int SettingsLastName = 2130968579;
+			
+			// aapt resource value: 0x7f040007
+			public const int SettingsLocationAuto = 2130968583;
 			
 			static String()
 			{
