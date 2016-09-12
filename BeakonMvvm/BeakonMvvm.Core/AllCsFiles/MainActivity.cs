33eec5b23bdefcba6ad52ad01d74912bba74c4b4
Ian <maskellx@gmail.com>
2016-09-13 09:56:42 +1000

    aa

--- BeakonMvvm/BeakonMvvm.Core/AllCsFiles/MainActivity.cs
+++ BeakonMvvm/BeakonMvvm.Core/AllCsFiles/MainActivity.cs
@@ -1,27 +1,28 @@
-﻿//using System;
-//using Android.App;
-//using Android.Content;
-//using Android.Runtime;
-//using Android.Views;
-//using Android.Widget;
-//using Android.OS;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿/*using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using System.Linq;
 
-//namespace Beakon
-//{
-
-
-//    [Activity(Label = "Beakon", MainLauncher = true, Icon = "@drawable/Beakon_Icon")]
-//    public class Main : Activity
-//    {
-//        protected override void OnCreate(Bundle bundle)
-//        {
-//            base.OnCreate(bundle);
-//            StartActivity(typeof(NotificationActivity));
-
-//        }
+namespace Beakon
+{
 
 
-//    }
-//}
+    [Activity(Label = "Beakon", MainLauncher = true, Icon = "@drawable/Beakon_Icon")]
+    public class Main : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            StartActivity(typeof(NotificationActivity));
+
+        }
+
+
+    }
+}
+*/
