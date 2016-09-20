fe97b43d7b21818fccb1ca6065cf9597efdd17c1
Ian <maskellx@gmail.com>
2016-09-20 10:32:52 +1000

    Please God be fixed

---
+++ BeakonMvvm/BeakonMvvm.Core/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Core.ViewModels;
+using System.Collections.ObjectModel;
+
+namespace BeakonMvvm.Core.ViewModels
+{
+   public class NotificationViewModel : MvxViewModel
+    {
+        public MvxCommand NavReqCmd
+        {
+            get
+            {
+                return new MvxCommand(() => ShowViewModel<RequestsViewModel>());
+            }
+        }
+
+        public MvxCommand NavSetCmd
+        {
+            get
+            {
+                return new MvxCommand(() => ShowViewModel<SettingsViewModel>());
+            }
+        }
+
+    }
+    
+}
+
--- BeakonMvvm/BeakonMvvm.Droid/Views/Requests.cs
+++ BeakonMvvm/BeakonMvvm.Droid/Views/Requests.cs
@@ -7,13 +7,13 @@ using MvvmCross.Droid.Views;
 namespace BeakonMvvm.Droid.Views
 {
     [MvxViewFor(typeof(RequestsViewModel))]
-    [Activity(Label = "Notification")]
-    public class Notification : MvxActivity
+    [Activity(Label = "Requests")]
+    public class Requests : MvxActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.Notification);
+            SetContentView(Resource.Layout.requests);
         }
     }
 }
