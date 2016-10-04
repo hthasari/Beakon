8408ce0213eb7233b69a8607830a48e5bfde3fc7
Ian Maskell <maskellx@hotmail.com>
2016-10-04 14:47:40 +1000

    Latest Commit by Ian

---
+++ BeakonMvvm/BeakonMvvm.Core/Classes/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeakonMvvm.Core.Classes
+{
+    class User
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        private bool _autoCalendar;
+        private bool _autoLocation;
+
+        public User(string firstName, string lastName, string email, bool autoCalendar, bool autoLocation)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+            _autoCalendar = autoCalendar;
+            _autoLocation = autoLocation;
+        }
+
+        public string UserFirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string UserLastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string UserEmail
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public bool UserAutoCalendar
+        {
+            get { return _autoCalendar; }
+            set { _autoCalendar = value; }
+        }
+        public bool UserAutoLocation
+        {
+            get { return _autoLocation; }
+            set { _autoLocation = value; }
+        }
+
+    }
+}
