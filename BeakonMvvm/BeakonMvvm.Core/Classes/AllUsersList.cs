664a0a0f0bfa069eb930d672e8a6cf33c669035b
Ian Maskell <maskellx@hotmail.com>
2016-10-10 21:50:29 +1000

    Latest Update

--- BeakonMvvm/BeakonMvvm.Core/Classes/AllUsersList.cs
+++ BeakonMvvm/BeakonMvvm.Core/Classes/AllUsersList.cs
@@ -11,15 +11,15 @@ namespace BeakonMvvm.Core.Classes
     {
         static private ObservableCollection<User> allUsers = new ObservableCollection<User>()
             {
-                new User("Aaron", "Andersen", "AAndersen1@hotmail.com"),
-                new User("Bill", "Benson", "BBensen1@hotmail.com"),
-                new User("Chevy", "Carter", "CCarter1@hotmail.com"),
-                new User("Derek", "Davis", "DDavis1@hotmail.com"),
-                new User("Erin", "Ericson", "EEricson1@hotmail.com"),
-                new User("Fred", "Farrell", "FFarrell1@hotmail.com"),
-                new User("George", "Grinch", "GGrinch1@hotmail.com"),
-                new User("Harri", "Houdini", "HHoudini1@hotmail.com"),
-                new User("Zed", "Zakowski", "ZZakowski@hotmail.com")
+                new User("0001", "Aaron", "Andersen", "AAndersen1@hotmail.com"),
+                new User("0002", "Bill", "Benson", "BBensen1@hotmail.com"),
+                new User("0003", "Chevy", "Carter", "CCarter1@hotmail.com"),
+                new User("0004","Derek", "Davis", "DDavis1@hotmail.com"),
+                new User("0005", "Erin", "Ericson", "EEricson1@hotmail.com"),
+                new User("0006", "Fred", "Farrell", "FFarrell1@hotmail.com"),
+                new User("0007", "George", "Grinch", "GGrinch1@hotmail.com"),
+                new User("0008", "Harri", "Houdini", "HHoudini1@hotmail.com"),
+                new User("0009", "Zed", "Zakowski", "ZZakowski@hotmail.com")
             };
 
         public ObservableCollection<User> AllUsers
